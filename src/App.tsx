41b4c59754e89b4cd2830b9e1336665a600c48b0
lernya <usernamegg@mail.ru>
2023-08-04 15:10:15 +0300

    Add accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,85 @@
 import jsonData from "./source.json";
-import StudyFormList from "./StudyFormList";
+import {
+  Accordion,
+  AccordionBody,
+  AccordionHeader,
+  AccordionItem,
+} from "react-headless-accordion";
 
-type Props = {};
-
-const App = (props: Props) => {
+const App = () => {
   const dataArray = Object.entries(jsonData);
-  // console.log(dataArray[0][1]);
-
   return (
-    <ul>
-      {dataArray.map((item, index) => {
-        return (
-          <li key={index}>
-            {item[0]}
-            <StudyFormList data={item[1]} />
-          </li>
-        );
-      })}
-    </ul>
+    <Accordion>
+      {dataArray.map((item, index) => (
+        <AccordionItem key={index}>
+          {(open: boolean) => (
+            <>
+              <AccordionHeader>
+                <h3
+                  className={`accordion-title ${
+                    open ? "accordion-active" : ""
+                  }`}
+                >
+                  {item[0]}
+                </h3>
+              </AccordionHeader>
+
+              <AccordionBody>
+                {Object.entries(item[1]).map((innerItem, innerIndex) => (
+                  <AccordionItem key={innerIndex}>
+                    {(innerOpen: boolean) => (
+                      <>
+                        <AccordionHeader>
+                          <h3
+                            className={`accordion-title ${
+                              innerOpen ? "accordion-active" : ""
+                            }`}
+                          >
+                            {innerItem[0]}
+                          </h3>
+                        </AccordionHeader>
+
+                        <AccordionBody>
+                          <div className="accordion-body">
+                            {Object.entries(innerItem[1]).map(
+                              (groupItem, groupIndex) => {
+                                return (
+                                  <a
+                                    href={groupItem[1]}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    key={groupIndex}
+                                  >
+                                    {groupItem[0]}
+                                  </a>
+                                );
+                              }
+                            )}
+                          </div>
+                        </AccordionBody>
+                      </>
+                    )}
+                  </AccordionItem>
+                ))}
+              </AccordionBody>
+            </>
+          )}
+        </AccordionItem>
+      ))}
+    </Accordion>
+
+    // <ul>
+    //   {dataArray.map((item, index) => {
+    //     return (
+    //       <>
+    //         <li key={index} onClick={handleClick}>
+    //           {item[0]}
+    //         </li>
+    //         {visibility ? <StudyFormList data={item[1]} /> : null}
+    //       </>
+    //     );
+    //   })}
+    // </ul>
   );
 };
 

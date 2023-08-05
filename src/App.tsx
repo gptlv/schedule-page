605eaa41252ad333b18f0414ab5d7e837c3643f2
lernya <usernamegg@mail.ru>
2023-08-05 11:55:10 +0300

    Add buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,43 @@
 import jsonData from "./source.json";
-import {
-  Accordion,
-  AccordionBody,
-  AccordionHeader,
-  AccordionItem,
-} from "react-headless-accordion";
+import { Button, Collapse, Space, Layout } from "antd";
+
+const { Header, Content, Footer } = Layout;
+
+const items = Object.entries(jsonData).map(([category, programs]) => ({
+  key: category,
+  header: category,
+  content: (
+    <Collapse accordion>
+      {Object.entries(programs).map(([program, buttonData]) => (
+        <Collapse.Panel key={program} header={program}>
+          <Space direction="vertical">
+            {Object.entries(buttonData).map(([groupName, buttonLink]) => (
+              <Button block type="primary" href={buttonLink} target="_blank">
+                {groupName}
+              </Button>
+            ))}
+          </Space>
+        </Collapse.Panel>
+      ))}
+    </Collapse>
+  ),
+}));
 
 const App = () => {
-  const dataArray = Object.entries(jsonData);
   return (
-    <Accordion>
-      {dataArray.map((item, index) => (
-        <AccordionItem key={index}>
-          {(open: boolean) => (
-            <>
-              <AccordionHeader>
-                <h3
-                  className={`accordion-title ${
-                    open ? "accordion-active" : ""
-                  }`}
-                >
-                  {item[0]}
-                </h3>
-              </AccordionHeader>
-
-              <AccordionBody>
-                {Object.entries(item[1]).map((innerItem, innerIndex) => (
-                  <AccordionItem key={innerIndex}>
-                    {(innerOpen: boolean) => (
-                      <>
-                        <AccordionHeader>
-                          <h3
-                            className={`accordion-title ${
-                              innerOpen ? "accordion-active" : ""
-                            }`}
-                          >
-                            {innerItem[0]}
-                          </h3>
-                        </AccordionHeader>
-
-                        <AccordionBody>
-                          <div className="accordion-body">
-                            {Object.entries(innerItem[1]).map(
-                              (groupItem, groupIndex) => {
-                                return (
-                                  <a
-                                    href={groupItem[1]}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    key={groupIndex}
-                                  >
-                                    {groupItem[0]}
-                                  </a>
-                                );
-                              }
-                            )}
-                          </div>
-                        </AccordionBody>
-                      </>
-                    )}
-                  </AccordionItem>
-                ))}
-              </AccordionBody>
-            </>
-          )}
-        </AccordionItem>
-      ))}
-    </Accordion>
-
-    // <ul>
-    //   {dataArray.map((item, index) => {
-    //     return (
-    //       <>
-    //         <li key={index} onClick={handleClick}>
-    //           {item[0]}
-    //         </li>
-    //         {visibility ? <StudyFormList data={item[1]} /> : null}
-    //       </>
-    //     );
-    //   })}
-    // </ul>
+    <Layout style={{ gap: "0.75rem", background: "none" }}>
+      <Header style={{ background: "#F5F5F5" }}>Учебное расписание</Header>
+      <Content>
+        <Collapse accordion>
+          {items.map((item) => (
+            <Collapse.Panel key={item.key} header={item.header}>
+              {item.content}
+            </Collapse.Panel>
+          ))}
+        </Collapse>
+      </Content>
+      <Footer>2023</Footer>
+    </Layout>
   );
 };
 

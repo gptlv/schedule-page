f0ef0af53c83bfb14a2e6f3db900bb1bc9b33264
lernya <usernamegg@mail.ru>
2023-08-15 14:49:39 +0300

    Edit vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react(), ssr()],
   base: "/schedule-page",
-});
+};

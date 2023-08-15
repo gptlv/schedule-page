fb7a3f623a789101e3b7ac3670e5dcdee57748d6
lernya <usernamegg@mail.ru>
2023-08-15 14:59:41 +0300

    Add prerender

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 
 // https://vitejs.dev/config/
-export default {
-  plugins: [react(), ssr()],
-  base: "/schedule-page",
-};
+export default defineConfig({
+  plugins: [react(), ssr({prerender: true})],
+  base: "/schedule-page/",
+});

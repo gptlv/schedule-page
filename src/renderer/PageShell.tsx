90b22be4acb17009cac41dc80814d923a9117921
gptlv <usernamegg@mail.ru>
2023-08-17 12:46:47 +0300

    Fix firefox issue

--- src/renderer/PageShell.tsx
+++ src/renderer/PageShell.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { PageContextProvider } from './usePageContext'
-import type { PageContext } from './types'
-import './PageShell.css'
+import React from "react";
+import { PageContextProvider } from "./usePageContext";
+import type { PageContext } from "./types";
+import "./PageShell.css";
 import { ConfigProvider, Layout } from "antd";
 
 import Header from "../components/Header";
@@ -9,45 +9,51 @@ import Main from "../components/Main";
 // import Footer from "../components/Footer";
 import { PRIMARY_COLOR } from "./../shared/colors";
 
-export { PageShell }
+export { PageShell };
 
-const PageShell = ({ children, pageContext }: { children: React.ReactNode; pageContext: PageContext }) => {
+const PageShell = ({
+  children,
+  pageContext,
+}: {
+  children: React.ReactNode;
+  pageContext: PageContext;
+}) => {
+  const [mounted, setMounted] = React.useState(false);
 
-const [mounted, setMounted] = React.useState(false);
-React.useEffect(() => setMounted(true), []);
-
-if (typeof window !== 'undefined') {
-    window.onload = () => {
-        document.getElementById('react-root')!.remove();
-    };
-}
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
 
   return (
     <React.StrictMode>
-        <PageContextProvider pageContext={pageContext}>
-<ConfigProvider
-      theme={{
-        token: {
-          fontFamily: "Noah",
-          colorPrimary: PRIMARY_COLOR,
-        },
-      }}
-    >
-      <div style={{ opacity: !mounted ? 0 : 1 }}>
-
-            
-      <Layout
-        style={{
-          minHeight: "100vh",
-        }}
-      >
-        <Header />
-        <Main>{children}</Main>
-        {/* <Footer /> */}
-      </Layout>
-      </div>
-    </ConfigProvider>
-    </PageContextProvider>
+      <PageContextProvider pageContext={pageContext}>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "Noah",
+              colorPrimary: PRIMARY_COLOR,
+            },
+          }}
+        >
+          <div
+            style={{
+              opacity: !mounted ? 0 : 1,
+              transition: "opacity 0.3s ease-in-out",
+            }}
+          >
+            <Layout
+              style={{
+                minHeight: "100vh",
+                // transition: "height 0.5s ease-in-out",
+              }}
+            >
+              <Header />
+              <Main>{children}</Main>
+              {/* <Footer /> */}
+            </Layout>
+          </div>
+        </ConfigProvider>
+      </PageContextProvider>
     </React.StrictMode>
   );
 };
